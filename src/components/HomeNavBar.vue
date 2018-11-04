1e2a2e7c6bf6b42e9ac3903f2c2201dd17f9fa31
Alex Smith <smithavt14@gmail.com>
2018-11-04 17:05:37 +0800

    Created the scss style file and began creating the home navbar component

---
+++ src/components/HomeNavBar.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  name: 'HomeNavBar',
+  props: {
+  },
+
+  data() {
+    return {
+    };
+  },
+
+  beforeMount() {
+  },
+};
+</script>
+
+<template>
+  <div class="HomeNavBar__wrapper">
+    <div class="HomeNavBar__logo"></div>
+    <div class="HomeNavBar__tabs">
+      <router-link to="/">HOME</router-link>
+      <router-link to="/about">ABOUT</router-link>
+      <a href="">EXPERIENCE</a>
+      <a href="">PROGRAMS</a>
+      <a href="">APPLY</a>
+      <div class="HomeNavBar__language-selector">中文</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../assets/styles.scss';
+
+.HomeNavBar__wrapper {
+  height: 150px;
+  width: 100vw;
+  background: $concord-orange;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.HomeNavBar__logo {
+  background-image: url('../assets/images/logo.png');
+  height: 150px;
+  width: 175px;
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  margin: 0 20px;
+}
+
+.HomeNavBar__tabs {
+  display: flex;
+  justify-content: space-around;
+  margin: 0 20px;
+  a {
+  color: black;
+  text-decoration: none;
+  margin: 0 20px
+  }
+
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HomeNavBar></HomeNavBar>
+    <HomePage></HomePage>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomePage from '@/components/homepage.vue';
+import HomeNavBar from '@/components/HomeNavBar.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
-  }
-}
+    HomeNavBar,
+    HomePage,
+  },
+};
 </script>
