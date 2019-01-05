8575c03dc644304b2b7414fb6a40ae7c55f30aac
Alex Smith <smithavt14@gmail.com>
2019-01-05 18:09:32 +0800

    Created the calendar component

--- src/translations/programs.js
+++ src/translations/programs.js
@@ -4,14 +4,16 @@ const programs = {
     earlyYears: 'EARLY-YEARS PROGRAM',
     privateLessons: 'PRIVATE LESSONS',
     groupLessons: 'GROUP LESSONS',
-    camps: 'CAMPS'
+    camps: 'CAMPS',
+    calendarTitle: 'CALENDAR',
   },
   Chinese: {
   	calendar: '日历',
     earlyYears: '要翻译',
     privateLessons: '要翻译',
     groupLessons: '要翻译',
-    camps: '要翻译'
+    camps: '要翻译',
+    calendarTitle: '日历',
   }
 }
 
--- src/views/Programs.vue
+++ src/views/Programs.vue
@@ -16,11 +16,79 @@ export default {
   },
 
   mounted() {
-   $('#calendar').fullCalendar({
-     defaultView: 'month',
-     aspectRatio: 1.5,
-     render: true
-   })
+    $('#calendar').fullCalendar({
+      aspectRatio: 1,
+      header: {
+        left: 'title',
+        center: '',
+        right: 'today, prev,next'
+      },
+      defaultView: 'month',
+      height: 415,
+      eventSources: [
+        {
+          events: [
+            {
+              title: 'Holiday',
+              start: '2019-02-02',
+              end: '2019-02-10'
+            },
+            {
+              title: 'Holiday',
+              start: '2019-05-01'
+            },
+            {
+              title: 'Holiday',
+              start: '2019-06-07'
+            },
+            {
+              title: 'Holiday',
+              start: '2019-07-22',
+              end: '2019-08-04'
+            }
+          ],
+          color: 'red',
+          textColor: 'white'
+        },
+        {
+          events: [
+            {
+              title: 'Spring Music Camp',
+              start: '2019-04-01',
+              end: '2019-04-05'
+            }
+          ],
+          color: 'orange',
+          textColor: 'white'
+        },
+        {
+          events: [
+            {
+              title: 'Recitals',
+              start: '2019-05-26'
+            }
+          ],
+          color: 'blue',
+          textColor: 'white'
+        },
+        {
+          events: [
+            {
+              title: 'Summer Music Camp',
+              start: '2019-06-24',
+              end: '2019-07-19'
+            },
+            {
+              title: 'Summer Music Camp',
+              start: '2019-08-05',
+              end: '2019-08-16'
+            }
+          ],
+          color: 'green',
+          textColor: 'white'
+        }
+      ]
+    })
   }
 }
 </script>
@@ -37,12 +105,16 @@ export default {
       <div class="programs__banner-bar-link">{{translation.groupLessons}}</div>
       <div class="programs__banner-bar-link">{{translation.camps}}</div>
     </div>
-    <div id='calendar'></div>
+    <div class="programs__calendar dynamic-width">
+      <div class="programs__calendar-title">{{translation.calendarTitle}}</div>
+      <div id='calendar'></div>  
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @import '../assets/styles.scss';
+@import '../assets/fullcalendar.css';
 
 .programs__wrapper {
   background-image: url('../assets/images/symphony.png');
@@ -73,25 +145,55 @@ export default {
   align-items: center;
 }
 
-#calendar {
-  height: 500px;
-  width: 500px;
-
-}
-
 .programs__banner-bar-link-active {
   border-bottom: 3px solid white;
+}
+
+.programs__calendar-title {
+  color: $concord-orange;
+  font-size: 15px;
+  margin: 5px;
+}
+
+.programs__calendar {
+  margin: 25px 0;
+  width: 100vw;
+  height: 500px;
+  max-width: 1220px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: $light-gray;
 }
 
 @media screen and (min-width: 751px) {
   .programs__banner-bar-link {
     margin: 0 25px;
   }
+
+  #calendar {
+    width: 75%;
+    overflow: hidden;
+  }
+
+  .dynamic-width {
+    width: 80vw;
+  }
 }
 
 @media screen and (max-width: 750px) {
   .programs__banner-bar-link {
     display: none;
+  }
+
+  #calendar {
+    width: 95%;
+  }
+
+  .dynamic-width {
+    width: 100vw;
+    border: 0;
   }
 }
 
