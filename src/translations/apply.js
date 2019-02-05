5c84cd8ffcfc30a2811db7e44e82a6d7f4778c14
Alex Smith <smithavt14@gmail.com>
2019-02-05 14:33:18 +0800

    Added translations to apply page

--- src/translations/apply.js
+++ src/translations/apply.js
@@ -1,5 +1,6 @@
 const apply = {
   English: {
+    submit: 'Submit',
     application_form: 'APPLICATION FORM',
     title: 'APPLY TO CONCORD MUSIC',
   	student_name: 'Student\'s Name',
@@ -10,9 +11,16 @@ const apply = {
     parent_phone: 'Parent\'s Phone', 
     comments: 'Please put any additional comments here',
     bottom_banner_title: 'Give your child a wonderful start with music',
-    see_courses: 'SEE COURSES'
+    see_courses: 'SEE COURSES',
+    success: 'Submission Success!',
+    success_description: 'Thanks for your application, we\'ll be in touch with you shortly.',
+    success_btn: 'Got it',
+    error: 'Submission Error!',
+    error_description: 'Yikes, we\'ve got an error. Please contact our staff directly to submit your application.',
+    error_btn: 'OK'
   },
   Chinese: {
+    submit: '确认',
     application_form: '申请表',
     title: '申请搿艺音乐',
   	student_name: '学生姓名',
@@ -23,7 +31,13 @@ const apply = {
     parent_phone: '家长手机号码', 
     comments: '其他信息',
     bottom_banner_title: '要翻译',
-    see_courses: '看更多'
+    see_courses: '看更多',
+    success: '申请成功！',
+    success_description: '谢谢您的申请，请稍等我们很快跟你联系',
+    success_btn: '确认',
+    error: '申请失败',
+    error_description: '哇 很抱歉，你直接跟我们客服联系',
+    error_btn: 'OK'
   }
 }
 export default apply
