f5a2d0104afb7fc5066dd64133a8c0e7d7af6214
Alex Smith <smithavt14@gmail.com>
2019-04-03 20:13:39 +0800

    Temporarily add English content to home page/chinese

--- src/translations/home.js
+++ src/translations/home.js
@@ -17,12 +17,12 @@ const home = {
     bannerBarTitle: '音乐项目',
     bannerBarText: '(还没翻译)',
     learnMore: '了解',
-    WAWsubtitle: '要翻译',
-    WAWtitle: '要翻译',
-    WAWcontent: '要翻译',
-    WAWsignup: '要翻译',
+    WAWsubtitle: 'WHO ARE WE?',
+    WAWtitle: 'Study with Shanghai\'s Best - Concord Music',
+    WAWcontent: 'Concord Music strives to provide a high level of music education that encourages students of all ages and levels to develop their interest in music. Our faculty consists of world-class musicians who have graduated from the world\'s most prestigious conservatories (Manhattan School of Music, NYU) and are able to bring their Western teaching expertise for the benefit of the Shanghai community. We are passionate about helping those who dream of learning a music instrument.',
+    WAWsignup: 'Sign up',
     FOBtitle: '来自于我们微信账号',
-    OPtitle: '我们项目',
+    OPtitle: '我们课程',
     loading: '加载中...'
   }
 }
