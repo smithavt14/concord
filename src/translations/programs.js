28c451082bc314831564f9ea5b6a246a6dc3cc41
Alex Smith <smithavt14@gmail.com>
2019-05-28 16:28:42 +0700

    Changed translation of text

--- src/translations/programs.js
+++ src/translations/programs.js
@@ -49,7 +49,7 @@ const programs = {
     schedule: 'SCHEDULE',
     year: 'Year 2019 (March - June)',
     pocoTitle: '"Poco A Poco" Piano Foundations & Musicianship Class',
-    schoolCalendar: 'Click here to see our school calendar'
+    schoolCalendar: "Click here to see Concord's music calendar"
   },
   Chinese: {
     programs: '课程',
