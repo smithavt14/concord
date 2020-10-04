bccd7574a2bd159c2f3582107bf27e6f9acd7fa7
Alex Smith <smithavt14@gmail.com>
2020-10-04 12:09:20 +0800

    Added additional photos; changed photos of programs

--- src/objects/programs_programs.js
+++ src/objects/programs_programs.js
@@ -6,7 +6,7 @@ const programs = {
         age: '(18 to 30 mos)',
         description: '“Baby Notes” is a stepping stone to our Little Notes Music Foundation Class for age 3-4. “Baby Notes” give mommies and toddlers the space to explore different sound and music through catchy children tunes, percussion instruments, story telling and movements. Our teachers are experts on early music education who are able to connect with young children and our lessons are fun and filled with dynamic. Many activities in our class can be done at home with your child, bringing the interest of music beyond classroom to your home. ',
         photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/programs__baby-notes.jpg'
-      }, 
+      },
       {
         title: '"Little Notes" Music Foundation',
         age: '(Age 3 to 4)',
@@ -16,9 +16,9 @@ const programs = {
       {
         title: '"Little Notes" + ',
         age: '(Age 3 to 4) ',
-        description: 'This course runs as workshop-setting classes based on the “Little Notes” Music Foundation course syllabus but as a condensed 12-14 weeks course. It not only gives children a start in music in the way of exploration, it also involves parents in the classes and gives parents some ideas about how to extend the music interest beyond classroom and continues the interest cultivation at home.This course runs as workshop-setting classes based on the “Little Notes” Music Foundation course syllabus but as a condensed 12-14 weeks course. It not only gives children a start in music in the way of exploration, it also involves parents in the classes and gives parents some ideas about how to extend the music interest beyond classroom and continues the interest cultivation at home.  \n\n What’s the “+”? \n \n + more instrumental exploration \n + weekly parent-child home projects  \n+ instrument demonstration by teachers  \n+ selected favorite songs from “Little Notes”  ',
+        description: 'This course runs as workshop-setting classes based on the “Little Notes” Music Foundation course syllabus but as a condensed 12-14 weeks course. It not only gives children a start in music in the way of exploration, it also involves parents in the classes and gives parents some ideas about how to extend the music interest beyond classroom and continues the interest cultivation at home.This course runs as workshop-setting classes based on the “Little Notes” Music Foundation course syllabus but as a condensed 12-14 weeks course. It not only gives children a start in music in the way of exploration, it also involves parents in the classes and gives parents some ideas about how to extend the music interest beyond classroom and continues the interest cultivation at home. \n\n What’s the “+”? \n \n + more instrumental exploration \n + weekly parent-child home projects  \n+ instrument demonstration by teachers  \n+ selected favorite songs from “Little Notes” ',
         photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/programs__little-notes.png'
-      }, 
+      },
       {
         title: '“Little Beethoven” Ear Training ',
         age: '(Age 4 to 5)',
@@ -30,43 +30,43 @@ const programs = {
       {
         title: 'Songs Around the World ',
         age: '(Age 4 to 5)',
-        description: 'This is a course created to build little global citizens who would discover their interest in singing and to appreciate different cultures. Music is an universal language and through singing, children will explore different countries, cultures, people and language. They will be singing songs from different countries and express their understandings of the cultures through singing. Each week, they will have take-home lyrics and MP3. This class is taught by our professional choral expert from Vienna who seek to inspire children to sing with beautiful voice! ',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/songs-around-the-world.jpeg', 
+        description: 'This is a course created to build little global citizens who would discover their interest in singing and to appreciate different cultures. Music is a universal language and through singing, children will explore different countries, cul- tures, people and language. They will be singing songs from different countries and explore the music, instrument, musicians and culture from different countries. This is a good introductory class to begin singing appreciation and to get to know your voice. With the fun theme of going around the world, the course is able to capture young children’s interest and learning motivation.',
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/around-the-world.jpeg',
         id: 1
       },
       {
         title: '1-on-4 Keyboard',
-        age: '(Age 3.5 to 4.5)', 
+        age: '(Age 3.5 to 4.5)',
         description: 'My Musical Animals is a method book written by Concord director for young beginners of age 3 to 4 to begin their interest in piano learning. With 1-on-4 group setting, each student will be getting guided hands-on experience on keyboard by our professional piano teacher. The learning scope of this course include pre-stave reading, listening, rhythm and playing technique. Off-piano activities in class makes the learning more dynamic and interactive as well, instead of just sitting for the whole lesson. The syllabus follows the 12 chapters in the method book (the book comes with a CD) which is easy for young children to follow and also quite user friendly for the parents to guide their child at home. After completing this course, students will acquire the basics of piano playing techniques which serves as a foundation to 1-on-1 piano studies.',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/1-on-4-keyboard.JPG', 
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/1-on-4-keyboard.JPG',
         id: 2
       },
       {
         title: '1-on-4 Violin',
         age: '(Age 4 to 5) ',
         description: 'Children will be guided by our foreign teachers, using innovative western teaching method and materials to learn the basic foundations of violin. The course covers reading, basic technique and theory learning. Through ensemble playing and group motivation, this course is a perfect start for young beginners! Performing opportunity will be provided to increase students’ confidence too. This course will use a British method book published by ABRSM, combined with other supporting materials selected by the instructor. Student will be ready to continue with 1-on-1 lessons with better confidence and interest after completion of this course.',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/apply__student-violen-teacher.jpg', 
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/violen.png',
         id: 3
       },
       {
-        title: 'Poco a Poco Piano Foundation & Musicianship Class', 
+        title: 'Poco a Poco Piano Foundation & Musicianship Class',
         age: '(Age 4.5 to 6) ',
         description: 'A 24-weeks course that gives young children a start in piano learning that teaches not only on the foundational and playing skills but also to nurture little musicians who have good understanding of music. Just as Poco a poco means in Italian music term “little by little”, this course provides step-by-step learning in piano and music. Each 60-minutes lesson is divided into two parts: piano learning and musicianship learning. For piano foundational training, we will help students to earn the piano playing skills such as rhythm, reading music, playing and listening. For musicianship learning, students will maximize their musical potential through classical music, great paintings, movement, and creative activities. Each student will also receive extra 1-on-1 guidance from the teacher so the teacher can help with specific technical needs of each individual and provide detailed feedback about each student’s progress. The course philosophy is simple: a good pianist is first and foremost, a good musician.',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/poco-poco.JPG', 
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/poco-poco.JPG',
         id: 4
       },
       {
         title: 'Ukulele Group Class',
         age: '(Age 4 to 5)',
         description: 'Ukulele is a fun and relaxing instrument that is easy for young children to pick up. This class also serves as a foundation class for young children to establish a firm interest with foundation before beginning 1-on-1 ukulele and guitar lessons. Course will be conducted in a fun group setting for children to learn foundation skills such as reading skills, sing and strumming skill, music theory, rhythm exercise and ensemble skill. Classes are conducted in a fun and relaxing style, students will acquire basic foundations through fun songs. ',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/about__banner.jpg', 
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/about__banner.jpg',
         id: 5
       },
       {
         title: '1-on-2 Instrumental Lessons',
         age: '(Age 4 to 5)',
         description: '1-on-2 instrumental studies is especially effective for young beginners aged 4 to 5. We encourage siblings of similar ages or best buddies to try this out. Off-instrument games and ensemble playing will be included to make the learning more interactive. Through sharing the learning process with another friend, it is also more fun and motivated. This can also work as parent-child lessons together!  \n\n 1-on-2 Instrument Courses: piano, violin, ukulele, guitar, vocal',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/experience__annie-violen.jpg', 
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/experience__annie-violen.jpg',
         id: 6
       }
     ],
@@ -82,13 +82,13 @@ const programs = {
         title: 'Choir Preparatory Course',
         age: '(Age 5 to 6)',
         description: 'For those children who love singing and plan to eventually join a choir, this is a good preparation and also a good way to begin one’s interest in singing. Singing in an ensemble is more fun than singing alone! Children will listen better to each other and they will learn to gain confidence through peer encouragement. Under the guidance of our professional foreign teacher, children will learn proper technique in singing, to appreciate good voice and to enjoy expressing themselves through singing. Children will also be equipped with the choral foundational skills such as following conductor’s cue, simple score reading and feeling the rhythm before they will be ready to join a choir. ',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/choir-prep-banner.jpeg'
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/choir.png'
       },
       {
         title: 'Concord Choir',
         age: '(Age 6 to 9)',
         description: 'Concord welcomes all singing lovers to join our choir! This is a long-term project to cultivate an interest in singing. After passing an audition, students will be involved in stages-based training with regular performance opportunities as motivation. Each group of choir may continue up to two years, consists of 6 stages or 6 terms of training. During the training, students will also be learn to work as a team and learn the foundations of choral singing together. Training includes sight singing, ear training, breathing exercise, diction and harmonization singing.',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/home__concord-choir.jpg'
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/concord-choir.jpg'
       }
     ]
   },
@@ -99,7 +99,7 @@ const programs = {
         age: '18个月至30个月',
         description: '“宝贝音符” 是“小小音符”音乐启蒙课的入门课程。课程给孩子提供一个轻松愉快的空间去探索音乐和声音。通过有趣的歌曲鼓励孩子的早期音乐兴趣；通过打击乐器发现声音，通过故事和想象力去引导音乐赏析；通过律动和想象去感受音乐。课程的老师们都是音乐启蒙专家、拥有多年幼教育经验，通过小朋友喜欢的方式和，让音乐融入孩子们的成长过程中。我们的音乐课也是充满欢乐和乐趣的，让您和宝宝每周都期待过来社交的同时，一起分享音乐！此课程也教导妈妈们如何在家为孩子提供一个欢乐的音乐氛围，把音乐兴趣的种子带回家。',
         photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/programs__baby-notes.jpg'
-      }, 
+      },
       {
         title: '“小小音符” 音乐启蒙课',
         age: '3岁至4岁 ',
@@ -111,7 +111,7 @@ const programs = {
         age: '3岁至4岁 ',
         description: '这课程是个工作坊性质的课程，也是“小小音符”音乐启蒙课程的浓缩课程。课程将在12-14周内完成，给予孩子在音乐上有个探索性和自由去发掘的开始。小小音符+ 在上课时也有爸妈一同参与进行的环节，宗旨是让兴趣培养作为是一家人一同去探索的一件事，让音乐的兴趣延伸到家庭中。\n\n ”+“ 了什么？\n\n+ 更多的乐器探索  \n+ 每周的亲子活动作业  \n+ 课堂里的live乐器示范和演奏  \n+ “小小音符”里最受孩子们喜爱的音乐 ',
         photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/programs__little-notes.png'
-      }, 
+      },
       {
         title: '“小小贝多芬”听力课',
         age: '4岁至5岁',
@@ -123,19 +123,19 @@ const programs = {
       {
         title: '唱响世界',
         age: '4岁至5岁',
-        description: '该课程专为创建小小地球居民而设！他们将会通过音乐去探索自己的兴趣并共欣赏地球各个角落不一样的文化。音乐是这个地球的通用语，通过歌唱，居民们将会一起探索不一样的国家、文化、人文和语言。他们会学习各国民谣并通过音乐、律动、歌唱来表达他们对该文化的理解。每周，他们将把歌词和MP3带回家。该课程由来自维也纳的专业合唱导师授课，希望帮助孩子们学习如何唱的美，唱的动听。',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/songs-around-the-world.jpeg',
+        description: '该课程专为创建小小地球居⺠而设。他们将会通过音乐去探索自己的兴趣并共欣赏地球各个⻆落不一样的文化。音乐 是这个地球的通用语，通过歌唱，小朋友们将会一起探索不一样的国家、文化、人文和语言。他们会学习各国⺠谣并 认识不同国家的音乐，乐器，音乐家和独特文化。这是很好的歌唱入⻔课程，教会孩子们学习如何发现掌握自己的声 音，学习歌唱的赏析和基础歌唱技巧。加上课程的’环游世界’主题，课程始终可以保持一种新鲜感和孩子的热诚!',
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/around-the-world.jpeg',
         id: 1
       },
       {
         title: '1对4电子琴课',
-        age: '3岁半至4岁半', 
+        age: '3岁半至4岁半',
         description: '《我的音乐动物园》是由搿艺音乐总监本人创作编辑的一本教材，专为幼龄孩子（3至4岁）提供快乐的钢琴启蒙。在1对4的高质量学习环境下，老师将引导孩子们认识乐器，掌握技巧。学习范围包括：音符方向的阅读，听力训练，节奏和弹奏技巧。课堂将通过互动和游戏，让学习过程变得更有趣，而不是枯燥，只有静态的重复练习。教材（书自带CD）的12章学习内容允许孩子们有系统地接受基础训练，同时书里简易的’练琴指南’也帮助家长在家可进行督促。课程结束后，学生将有稳固的基础，为后续更专业和全面的一对一钢琴学习奠定基础。',
         photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/1-on-4-keyboard.JPG',
         id: 2
       },
       {
-        title: '“小步点” 钢琴启蒙', 
+        title: '“小步点” 钢琴启蒙',
         age: '4岁半至6岁',
         description: '这是一个为期24周的全新课程，专门为4.5岁-6岁的儿童提供一个钢琴学习的良好开端。该课程不仅单单训练孩子们的钢琴基础和演奏技能，而且还着重培养孩子对音乐的理解，将每一位儿童变成一个理解音乐的“小小音乐家”。就像Poco a Poco在意大利语中的意思是“一点一点的”，这门课程将引导每一位学生一步一步的了解钢琴和音乐。这门课程每节课将分成两部分：钢琴学习和音乐修养。这两个部分其实是相互关联的，音乐的修养可以帮助学生更好地了解他们演奏的音乐。对于钢琴基础训练，老师会逐步去教孩子们在钢琴演奏所需要的弹奏的技巧、键盘的认知和节奏性的学习。对于音乐修养的学习，老师将通过古典音乐、画作、肢体语言和创新活动来激发学生们的音乐潜能。',
         photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/poco-poco.JPG',
@@ -145,9 +145,9 @@ const programs = {
         title: '1对4小提琴课',
         age: '4岁至5岁',
         description: '孩子们将在外教的指导下，沿用创新的西方教学方法和教材学习小提琴的基本基础。课程包括阅读、基本技术和乐理知识。通过合奏和集体学习带来的动力，本课程是初学者完美的开始！同时也将提供表演机会，以增加学生的自信心。课程采用英皇出版社的英式教材，结合老师所挑选的其辅助教材。在完成这个课程后，学生将拥有充足的兴趣和信心去开始一对一的课程。',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/apply__student-violen-teacher.jpg',
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/violen.png',
         id: 4
-      }, 
+      },
       {
         title: '尤克里里集体课',
         age: '4岁至5岁',
@@ -175,13 +175,13 @@ const programs = {
         title: '合唱预备班',
         age: '5岁至6岁',
         description: '对于那些热爱歌唱并今后想要加入合唱团的孩子们，这是奠定合唱兴趣的一个良好的开端。在众多歌唱形式中，合唱独唱更加的具有趣味性！他们可以更好地彼此倾听，并且通过同伴的鼓励和肯定获取更多的自信息，从而更好地表达自我，展现自我。在搿艺专业的外籍教师的指导下，孩子们将学习专业的歌唱技巧和方式，欣赏更加纯粹的声音，并通过歌唱表达自己。孩子们还将配备合唱基本技能，比如听从指挥的提示，跟随准确地节奏去进行读谱，为以后加入合唱团而做好准备。',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/choir-prep-banner.jpeg'
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/choir.png'
       },
       {
         title: '搿艺合唱团',
         age: '6岁至9岁',
         description: '搿艺欢迎爱唱歌的小朋友加入搿艺合唱! 这是搿艺为了培养歌唱兴趣的一个长期计划。在通过面试后，老师将按该组的年龄和水平开始进行阶段性训练，并以定期的演奏机会将帮助提升学生的动力。每组的合唱一旦开班，将持续两年的长期计划学习，以1至6阶段（6个学期）进行阶段性训练。学生在学习过程中，除了学会歌唱技巧，也可以培养合唱的团体精神。阶段性训练将涵盖多方面训练：视唱练耳、呼吸练习、语音能力训练以及和声训练。',
-        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/home__concord-choir.jpg'
+        photo: 'https://concord-assets.oss-cn-beijing.aliyuncs.com/201003%20updates/concord-choir.jpg'
       }
     ]
   }
